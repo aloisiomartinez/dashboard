0959d50f674546c82114b05c095142ac61e8f03e
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-01-15 17:29:33 -0300

    typing themes styled.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+// Libera os temas da aplicação disponíveis para todos os componentes
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
