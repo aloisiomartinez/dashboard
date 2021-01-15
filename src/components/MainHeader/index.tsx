f52e566ff2bff483333ceb2cb4f814fca7718c8a
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-01-15 17:53:00 -0300

    useMemo for emojis

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+
+import emojis from '../../utils/emojis';
 
 import * as S from './styles';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length); // Total do array de  Emojis (0 - 6)
+    return emojis[indice];
+  }, []);
+
   return (
     <S.Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+      <S.Profile>
+        <S.Welcome>Olá, {emoji}</S.Welcome>
+        <S.UserName>Aloisio Martinez</S.UserName>
+      </S.Profile>
     </S.Container>
   );
 };
