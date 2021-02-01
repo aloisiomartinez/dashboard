2b8bbd0324b414215bd9f735577fa4b1e5fe1097
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-02-01 11:52:06 -0300

    starting signIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/auth';
+
+import * as S from './styles';
 
 const SignIn: React.FC = () => {
-  return <h1>SignIn</h1>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const { signIn } = useAuth();
+
+  return (
+    <S.Container>
+      <S.Logo>
+        <img src={logoImg} alt='Minha Carteira' />
+        <h2>Minha Carteira</h2>
+      </S.Logo>
+
+      <S.Form onSubmit={() => signIn(email, password)}>
+        <S.FormTitle>Entrar</S.FormTitle>
+
+        <Input type='email' placeholder='e-mail' required onChange={e => setEmail(e.target.value)} />
+        <Input type='password' placeholder='senha' required onChange={e => setPassword(e.target.value)} />
+
+        <Button type='submit'>Acessar</Button>
+      </S.Form>
+    </S.Container>
+  );
 };
 
 export default SignIn;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app.routes';
+import Auth from './auth.routes';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
