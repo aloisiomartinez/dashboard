4460153b58b5b44eecc40547f015fe9f28af63e0
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-02-02 15:55:56 -0300

    adding media queries on wallet box

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -15,4 +15,11 @@ export const Grid = styled.div`
   grid-template-areas: 'AS MH' 'AS CT';
 
   height: 100vh;
+
+  @media (max-width: 600px) {
+    grid-template-columns: 100%;
+    grid-template-rows: 70px auto;
+
+    grid-template-areas: 'MH' 'CT';
+  }
 `;
--- src/components/WalletBox/index.tsx
+++ src/components/WalletBox/index.tsx
@@ -34,7 +34,8 @@ const WalletBox: React.FC<IWalletBoxProps> = ({ title, amount, footerLabel, icon
     <S.Container color={color}>
       <span>{title}</span>
       <h1>
-        <CountUp end={amount} prefix='R$ ' separator='.' decimal=',' decimals={2} />
+        <strong>R$ </strong>
+        <CountUp end={amount} separator='.' decimal=',' decimals={2} />
       </h1>
       <small>{footerLabel}</small>
       <img src={iconSelected} alt={title} />
