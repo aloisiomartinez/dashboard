19e1b72df0c8b61e313b266cb810bd8756ae7db5
aloisiomartinez <aloisiojacobs36@gmail.com>
2021-02-13 17:51:15 -0300

    history box responsive

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,22 +1,34 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 interface ILegendProps {
   color: string;
 }
 
+const animate = keyframes`
+    0% {
+        transform: translateX(-100px);
+        opacity: 0;
+    }
+    50%{
+        opacity: .3;
+    }
+    100%{
+        transform: translateX(0px);
+        opacity: 1;
+    }
+`;
+
 export const Container = styled.div`
   width: 100%;
-
   display: flex;
   flex-direction: column;
-
   background-color: ${props => props.theme.colors.tertiary};
   color: ${props => props.theme.colors.white};
-
   margin: 10px 0;
   padding: 30px 20px;
-
   border-radius: 7px;
+
+  animation: ${animate} 0.5s;
 `;
 
 export const ChartContainer = styled.div`
@@ -33,6 +45,10 @@ export const Header = styled.header`
   > h2 {
     margin-bottom: 20px;
     padding-left: 16px;
+  }
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
   }
 `;
 
@@ -62,5 +78,12 @@ export const Legend = styled.li<ILegendProps>`
 
   > span {
     margin-left: 5px;
+  }
+
+  @media (max-width: 1280px) {
+    > div {
+      width: 30px;
+      height: 30px;
+    }
   }
 `;
--- src/components/MessageBox/styles.ts
+++ src/components/MessageBox/styles.ts
@@ -22,35 +22,44 @@ export const Container = styled.div`
   border-radius: 7px;
   margin: 10px 0;
   padding: 30px 20px;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
   animation: ${animate} 0.5s;
+
   > header img {
     width: 35px;
     margin-left: 7px;
   }
+
   > header p {
     font-size: 18px;
   }
+
   @media (max-width: 770px) {
     width: 100%;
 
     > header h1 {
       font-size: 24px;
+
       img {
         height: 20px;
         width: 20px;
       }
     }
+
     > header p,
     > footer span {
       font-size: 14px;
     }
   }
+
   @media (max-width: 420px) {
     width: 100%;
     height: auto;
+
     > header p {
       margin-bottom: 15px;
     }
